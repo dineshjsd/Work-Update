22c450e6e6b7e79d3d1fe58980bbc4cc768a0f18
Dinesh Jsdc <dineshdhansu009@gmail.com>
2022-05-11 23:21:59 +0530

    starting react

---
+++ reactjs/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <h1>Starting React</h1>
+   <h2>Hello World</h2>
+   </>
+  );
+}
+
+export default App;
