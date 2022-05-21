0dec578eedf4f01d8ca6c21e344e828cd79a650e
Dinesh Jsdc <dineshdhansu009@gmail.com>
2022-05-21 13:12:04 +0530

    context api

---
+++ reactjs/Hooks  project/src/components/store/authContext.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInformation = localStorage.getItem('isLoggedIn');
+
+    if(storedUserLoggedInformation === '1'){
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1"); // store the login value in local storage
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn'); // remove login value
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.childer}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
