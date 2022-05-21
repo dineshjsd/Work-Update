944f64fb39cf235ae797aa260825399ec8612e89
Dinesh Jsdc <dineshdhansu009@gmail.com>
2022-05-21 21:33:42 +0530

    start food order app

---
+++ reactjs/food ordering project/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+function Header(props) {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meals on table" />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
