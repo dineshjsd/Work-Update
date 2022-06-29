d9a0183f53d1ae24047ab6d716c95ea19f9f597f
Dinesh Jsdc <dineshdhansu009@gmail.com>
2022-06-29 09:49:03 +0530

    modify viewscreen

--- react-nativ/DoneWithIt/app/screen/ViewImageScreen.js
+++ react-nativ/DoneWithIt/app/screen/ViewImageScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
+
+import color from "../config/colors";
 
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -15,8 +19,24 @@ function ViewImageScreen(props) {
 }
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
   container: {
-    backgroundColor: "#000",
+    backgroundColor: colors.black,
     flex: 1,
   },
   image: {
